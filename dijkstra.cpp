8d007d7a428b5c8f7cf8625b95fa8b9ac2efa95b
Frank0125 <a01252974@tec.mx>
2024-10-08 21:47:41 -0700

    Dijkstra Branch created

---
+++ dijkstra.cpp
@@ -0,0 +1,19 @@
+#include "iostream"
+
+using namespace std;
+
+// El programa debe leer un numero n seguido de n x n valores enteros 
+// no negativos que representan una matriz de adyacencias de un grafo dirigido.
+// El primer número representa el número de nodos, los siguientes 
+// valores en la matriz, el valor en la posición i,j representan el peso 
+// del arco del nodo i al nodo j. Si no hay un arco entre el nodo i y el 
+// nodo j, el valor en la matriz debe ser -1
+
+// La salida del programa es, primero con el algoritmo de Dijkstra 
+// la distancia del nodo i a l nodo j para todos los nodos, y luego, 
+// la matriz resultado del algoritmo de Floyd
+
+int dijkstra(){
+    cout << "Lets Code" << endl;
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include "iostream"
+#include "dijkstra.cpp"
 
 using namespace std;
 // g++ -o main main.cpp to create the executable
 
 int main(){
+    dijkstra();
     cout << "Lets Code" << endl;
     return 0;
 }
